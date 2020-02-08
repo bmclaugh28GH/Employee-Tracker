1840d8d057cb9c168a735c189a759d372d8ddc62
BrianMcL <bmclaugh28@yahoo.com>
2020-02-08 13:47:31 -0500

    data fixes

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 // **********************************************
 var inquirer = require ("inquirer"); 
 var mysql = require ("mysql"); 
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 8084;
 
 var connection = mysql.createConnection({
    host: "localhost",
@@ -16,13 +20,61 @@ connection.connect(function(err) {
    if (err) throw err;
    console.log("connected as id " + connection.threadId + "\n");
 
-   // startFunction(); 
+   interact();  
 
 });
 
 // **********************************************
 // functions  
 // **********************************************
+
+function interact(){
+
+   inquirer
+   prompt([
+      {
+         type:"list",
+         message:"What do you want to do?",
+         name:"action",
+         choices:["Add role", "Add department", "Add employee","View role", "View department","View employees","Update employee role"]
+         }
+   ])
+
+/*      {
+      type: "list",
+      message: "'Upload' a profile image", 
+      name: "profile_image", 
+      choices: ['u.jpg', 'ew.jpg', 'q.jpg', 'h.jpg']
+      },
+      // 
+      // ****************************************
+      // manager-specific
+      // ****************************************
+      {
+      type: "list",
+      message: "What is the manager's specialty?", 
+      name: "specialty", 
+      choices: ['Team Manager', 'QA Manager', 'Development Manager'],
+      when: function(answers) {
+      return answers.role=='Manager'; 
+      }
+      }, 
+*/
+
+/*      {
+         name: "choice",
+         type: "rawlist",
+         choices: function() {
+         var choiceArray = [];
+         for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].item_name);
+         }
+         return choiceArray;
+         },
+*/
+
+
+} // interact 
 
 // **********************************************
 // listeners 
@@ -31,4 +83,8 @@ connection.connect(function(err) {
 // **********************************************
 // init 
 // **********************************************
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
 
